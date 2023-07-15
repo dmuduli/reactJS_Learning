722d81fbeed38e26a42251493efd74a9b30bced9
Deepak Muduli <deepak.muduli@nagarro.com>
2023-07-15 11:15:36 +0530

    Use props function

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,49 @@
 import "./App.css";
 import ExpenseItem from "./Components/ExpenseItem/ExpenseItem";
-import Products from "./Components/Products/Products";
+import ProductItem from "./Components/Products/ProductItem";
 
 function App() {
+  const products = [
+    {
+      id: "p1",
+      title: "Product 1",
+      price: 30,
+      description: "First Product",
+    },
+    {
+      id: "p2",
+      title: "Product 2",
+      price: 50,
+      description: "Second Product",
+    },
+    {
+      id: "p3",
+      title: "Product 3",
+      price: 100,
+      description: "Third Product",
+    },
+  ];
   return (
     <div>
       <h2>My Demo Shop</h2>
-      <Products></Products>
+      <ProductItem
+        title={products[0].title}
+        price={products[0].price}
+        description={products[0].description}
+      />
+      <ProductItem
+        title={products[1].title}
+        price={products[1].price}
+        description={products[1].description}
+      />
+      <ProductItem
+        title={products[2].title}
+        price={products[2].price}
+        description={products[2].description}
+      />
+      {/* <ProductItem title="Product 1" price="30" description="First Product" />
+      <ProductItem title="Product 2" price="50" description="Second Product" />
+      <ProductItem title="Product 3" price="100" description="Third Product" /> */}
       <ExpenseItem></ExpenseItem>
     </div>
     // <div>
