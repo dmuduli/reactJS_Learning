140d84fbc437f6f5c55dbd1dc329c3f52ed1abe5
Deepak <dmuduli@yahoo.co.in>
2023-09-10 09:48:29 +0530

    Delete my-app directory

--- my-app/src/Components/Card/Card.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./Card.css";
-
-function Card(props) {
-  const classes = `card rounded-lg bg-gray-800 ${props.className}`;
-  return <div className={classes}>{props.children}</div>;
-}
-
-export default Card;
--- my-app/src/Components/Concepts/ConceptsCard/ConceptsCard.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./ConceptsCard.css";
-
-function ConceptsCard(props) {
-  const classes = `concepts-card ${props.className}`;
-  return <div className={classes}>{props.children}</div>;
-}
-
-export default ConceptsCard;
